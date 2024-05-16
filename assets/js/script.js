36f130d8dbaad9349d43765d86b3c52d1f10a8b9
Chris Tranter <135023953+Chris-Tranter@users.noreply.github.com>
2024-05-16 12:56:20 +0100

    added function to check for correct/incorrect answer and give the user an alert depending on input

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function() {
     for (let button of buttons) {
         button.addEventListener("click", function() {
             if (this.getAttribute("data-type") === "submit") {
-                alert("You clicked Submit!");
+                checkAnswer();
             } else {
                 let gameType = this.getAttribute("data-type");
                 runGame(gameType);
@@ -33,9 +33,21 @@ function runGame(gameType) {
         throw `Unknown game type: ${gameType}. Aborting!`;
     }
 }
-
+/**
+ * checks the answer against the first element of array: calculateCorrectAnswer
+ */
 function checkAnswer() {
+    let userAnswer = parseInt(document.getElementById("answer-box").value);
+    let calculatedAnswer = calculateCorrectAnswer();
+    let isCorrect = userAnswer === calculatedAnswer[0];
 
+    if (isCorrect) {
+        alert("Hey! Your clever :)");
+    } else {
+        alert(`Awwww.... you're not so clever! ${userAnswer} was incorrect! The correct answer was actually ${calculatedAnswer[0]}!`);
+    }
+
+    runGame(calculatedAnswer[1]);
 }
 
 /**
